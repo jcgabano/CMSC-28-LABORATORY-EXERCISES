4a58a8ba3d73cead9a6ce46b7e400dd4cfcf3803
jcgabano <jcgabano@up.edu.ph>
2024-05-07 20:00:49 +0800

    added PE05 file

---
+++ jcgabano_CS28_PE05.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int dataEntry() {
+    // asks the user to input integers and store them in an array (e.g. num), and stop the data entry process once the user has inputted 0 or the maximum size of the array has been fully used.  You may set the array size to 10.
+}
+
+int findLargest() {
+    // Your function should receive the array as a parameter and return a value to the calling program.  For the maximum and minimum values, do not use the built-in function of the array in finding these values but design your own user-defined functions.
+}
+
+int findSmallest() {
+    // Your function should receive the array as a parameter and return a value to the calling program.  For the maximum and minimum values, do not use the built-in function of the array in finding these values but design your own user-defined functions.
+}
+
+int dataAverage() {
+    // Your function should receive the array as a parameter and return a value to the calling program.
+}
+
+int dataSTDEV() {
+    // Your function should receive the array as a parameter and return a value to the calling program.
+}
+
+int dataVariance() {
+    // Your function should receive the array as a parameter and return a value to the calling program.
+}
+
+void outputDisplay() {
+    // The output function should return no value to the calling program but it should accept parameters for the five values.
+}
+
+main() {
+
+}
